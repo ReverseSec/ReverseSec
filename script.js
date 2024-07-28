1cd867c05a19639a2bdca005e400fe466f5f6f94
idkhidden <91305428+idkhidden@users.noreply.github.com>
2024-07-29 01:25:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const text = "ReverseSec -> Coming Soon!";
+const text = "ReverseSec specializes in vulnerability research to enhance application security -> Coming Soon!";
 const typingElement = document.getElementById('typingEffect');
 let index = 0;
 
 function typeEffect() {
     if (index < text.length) {
-        typingElement.innerHTML += text.charAt(index);
+        typingElement.innerHTML += text.charAt(index); 
         index++;
         setTimeout(typeEffect, 75); 
     }
 }
 
-typeEffect();
+
+setTimeout(typeEffect, 500);
